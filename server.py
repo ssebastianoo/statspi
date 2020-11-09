8edd4e96337594463f95e51df06f6d8f7f63834d
ssebastianoo <sebastianogirotto05@gmail.com>
2020-11-09 10:14:11 +0100

    init

---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+import datetime, psutil, platform, os
+
+class Stats:
+    def __init__(self):
+        self.launchtime = datetime.datetime.now()
+
+    def uptime(self):
+        "get raspberry uptime"
+
+        uptime = datetime.datetime.now() - self.launchtime
+        hours, x = divmod(int(uptime.total_seconds()), 3600)
+        minutes, seconds = divmod(x, 60)
+        days, hours = divmod(hours, 24)
+
+        return f"{days} days, {hours} hours, {minutes} minutes, {seconds} seconds"
+
+    def temperature(self):
+        temp = os.popen("vcgencmd measure_temp").readline()
+        return (temp.replace("temp=",""))
+
+    def get_stats(self):
+        "get all raspberry stats"
+
+        stats = {
+            "uptime": self.uptime(),
+            "memory": psutil.virtual_memory()[2],
+            "cpu": f"{psutil.cpu_percent()}%",
+            "node": platform.node(),
+            "system": platform.system(),
+            "machine": platform.machine(),
+            "architecture": f"{platform.architecture()[1]} {platform.architecture()[0]}",
+            "temperature": self.temperature()
+        }      
+        
+        return stats
+
+app = Flask(__name__)
+stats = Stats()
+
+@app.route("/")
+def index():
+    rasp_info = stats.get_stats()
+    stats_str = ""
+
+    for stat in rasp_info:
+        stats_str += f"{stat}: {rasp_info[stat]}<br>"
+
+    return stats_str
+
+if __name__ == '__main__':
+    app.run(debug=True)
